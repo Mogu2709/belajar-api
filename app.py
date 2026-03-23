aa1928579b7c2ef5c79bf3c960bd959b2e4a40c3
Mogu2709 <moguoffical.id@gmail.com>
2026-03-23 11:27:15 +0700

    fix port for railway

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 import sqlite3
+import os
 
 app = Flask(__name__)
 CORS(app)
@@ -58,4 +59,5 @@ def delete_skill(id):
 
 if __name__ == "__main__":
     init_db()
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
